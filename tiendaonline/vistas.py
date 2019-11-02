40c7797af310032f89df27bea85d760245975e27
danielcrusser <dan.cruisee@gmail.com>
2019-11-01 22:45:07 -0600

    tutorial pildoras informaticas

---
+++ tiendaonline/vistas.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.template.loader import get_template
+from django.http import HttpResponse
+
+def registro(request):
+    return render(request, "superior/registro.html")
+
+
